b0a8905efee15ce2c473b868eab891dd6187a11a
Jordan Smithson <jordantroysmithson@gmail.com>
2018-09-18 21:59:12 -0600

    dummy data done for all except animals_morphs table woot!

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,16 @@
-drop table if exists reptile_types;
+drop table if exists cart;
+drop table if exists users;
+drop table if exists animals_morphs;
+drop table if exists morphs;
+drop table if exists animals;
+drop table if exists reptile_types;  
+drop table if exists sub_types;
+
 create table reptile_types (
   id serial primary key,
   type text
 );
 
-drop table if exists sub_types;
 create table sub_types (
   id serial primary key,
   scientific_name text,
@@ -15,18 +21,17 @@ create table sub_types (
   exp_level text
 );
 
-drop table if exists animals;
 create table animals (
   id serial primary key,
   hatch_date date,
   gender text,
-  uniqui_id text not null,
+  unique_id text not null,
   type_id integer references reptile_types(id),
   sub_type_id integer references sub_types(id),
+  cost decimal,
   image text
 );
 
-drop table if exists users;
 create table users (
   id serial primary key,
   username varchar(30),
@@ -36,23 +41,23 @@ create table users (
   image text
 );
 
-drop table if exists cart;
 create table cart (
   id serial primary key,
   user_id integer references users(id),
   animal_id integer references animals(id)
 );
 
-drop table if exists morphs;
 create table morphs (
-  id serial primary key,
-  name text,
-  description text
+    id serial primary key,
+    morph_name text
 );
 
-drop table if exists animals_morphs;
 create table animals_morphs (
   id serial primary key,
   animal_id integer references animals(id),
   morph_id integer references morphs(id)
 );
+
+ 
+
+ 
