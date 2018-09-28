e073d7c78249498e7f56374f0f50f2e7278893ce
Jordan Smithson <jordantroysmithson@gmail.com>
2018-09-28 12:20:25 -0600

    added basic css to clearance

--- src/Components/Clearance/Clearance.js
+++ src/Components/Clearance/Clearance.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.section `
+  background: gray;
+  color: lightgreen
+  height: 100vh;
+  width: 100vww;
+  margin-top: 121px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  font-size: 50px;
+`;
 
 export default function Clearance () {
   return (
-    <div>
+    <Wrapper>
       <h1>Clearance Coming Soon!</h1>
       <h3>Please stay tuned for epic epics of epicness!</h3>
-    </div>
+    </Wrapper>
   )
 }
