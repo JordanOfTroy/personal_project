0c745e7e99ba64cac79d05809172b81d48cd39cb
Jordan Smithson <jordantroysmithson@gmail.com>
2018-10-05 16:09:07 -0600

    added styling to about.js

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,63 +5,63 @@ class About extends Component {
 
   render () {
     return (
-      <div className = 'about_main'>
+      <div className = 'dashboard_wrapper about_main'>
         <section>
           <div>
-            <h2>
+            <h2 className = 'question'>
               Who are we, anyway?
             </h2>
-            <p>
+            <p className = 'answer'>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, illo repudiandae! Velit repudiandae, culpa odit quibusdam nam assumenda quis eveniet, voluptas, distinctio provident soluta debitis optio tempora! Minus, harum eaque?
               Sunt, sapiente omnis, suscipit optio quo numquam debitis quisquam voluptatum quibusdam ad harum veniam sequi? Sapiente tempora mollitia omnis quos quis unde expedita, quae pariatur, doloremque maiores suscipit, atque nulla.
               Quae harum ab delectus beatae suscipit impedit, modi non ex optio eaque id ullam aut, sunt repudiandae nisi? Ad expedita explicabo architecto sapiente voluptas atque, rem aperiam dolor fugiat asperiores.
             </p>
-            <p>
+            <p className = 'answer'>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, illo repudiandae! Velit repudiandae, culpa odit quibusdam nam assumenda quis eveniet, voluptas, distinctio provident soluta debitis optio tempora! Minus, harum eaque?
               Sunt, sapiente omnis, suscipit optio quo numquam debitis quisquam voluptatum quibusdam ad harum veniam sequi? Sapiente tempora mollitia omnis quos quis unde expedita, quae pariatur, doloremque maiores suscipit, atque nulla.
               Quae harum ab delectus beatae suscipit impedit, modi non ex optio eaque id ullam aut, sunt repudiandae nisi? Ad expedita explicabo architecto sapiente voluptas atque, rem aperiam dolor fugiat asperiores.
             </p>
-            <p>
+            <p className = 'answer'>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, illo repudiandae! Velit repudiandae, culpa odit quibusdam nam assumenda quis eveniet, voluptas, distinctio provident soluta debitis optio tempora! Minus, harum eaque?
               Sunt, sapiente omnis, suscipit optio quo numquam debitis quisquam voluptatum quibusdam ad harum veniam sequi? Sapiente tempora mollitia omnis quos quis unde expedita, quae pariatur, doloremque maiores suscipit, atque nulla.
               Quae harum ab delectus beatae suscipit impedit, modi non ex optio eaque id ullam aut, sunt repudiandae nisi? Ad expedita explicabo architecto sapiente voluptas atque, rem aperiam dolor fugiat asperiores.
             </p>
           </div>
           <div>
-            <h2>
+            <h2 className = 'question'>
               Why we do what we do.
             </h2>
-            <p>
+            <p className = 'answer'>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, illo repudiandae! Velit repudiandae, culpa odit quibusdam nam assumenda quis eveniet, voluptas, distinctio provident soluta debitis optio tempora! Minus, harum eaque?
               Sunt, sapiente omnis, suscipit optio quo numquam debitis quisquam voluptatum quibusdam ad harum veniam sequi? Sapiente tempora mollitia omnis quos quis unde expedita, quae pariatur, doloremque maiores suscipit, atque nulla.
               Quae harum ab delectus beatae suscipit impedit, modi non ex optio eaque id ullam aut, sunt repudiandae nisi? Ad expedita explicabo architecto sapiente voluptas atque, rem aperiam dolor fugiat asperiores.
             </p>
-            <p>
+            <p className = 'answer'>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, illo repudiandae! Velit repudiandae, culpa odit quibusdam nam assumenda quis eveniet, voluptas, distinctio provident soluta debitis optio tempora! Minus, harum eaque?
               Sunt, sapiente omnis, suscipit optio quo numquam debitis quisquam voluptatum quibusdam ad harum veniam sequi? Sapiente tempora mollitia omnis quos quis unde expedita, quae pariatur, doloremque maiores suscipit, atque nulla.
               Quae harum ab delectus beatae suscipit impedit, modi non ex optio eaque id ullam aut, sunt repudiandae nisi? Ad expedita explicabo architecto sapiente voluptas atque, rem aperiam dolor fugiat asperiores.
             </p>
-            <p>
+            <p className = 'answer'>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, illo repudiandae! Velit repudiandae, culpa odit quibusdam nam assumenda quis eveniet, voluptas, distinctio provident soluta debitis optio tempora! Minus, harum eaque?
               Sunt, sapiente omnis, suscipit optio quo numquam debitis quisquam voluptatum quibusdam ad harum veniam sequi? Sapiente tempora mollitia omnis quos quis unde expedita, quae pariatur, doloremque maiores suscipit, atque nulla.
               Quae harum ab delectus beatae suscipit impedit, modi non ex optio eaque id ullam aut, sunt repudiandae nisi? Ad expedita explicabo architecto sapiente voluptas atque, rem aperiam dolor fugiat asperiores.
             </p>
           </div>
           <div>
-            <h2>
+            <h2 className = 'question'>
               Our long, draw out, and complicated history.
             </h2>
-            <p>
+            <p className = 'answer'>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, illo repudiandae! Velit repudiandae, culpa odit quibusdam nam assumenda quis eveniet, voluptas, distinctio provident soluta debitis optio tempora! Minus, harum eaque?
               Sunt, sapiente omnis, suscipit optio quo numquam debitis quisquam voluptatum quibusdam ad harum veniam sequi? Sapiente tempora mollitia omnis quos quis unde expedita, quae pariatur, doloremque maiores suscipit, atque nulla.
               Quae harum ab delectus beatae suscipit impedit, modi non ex optio eaque id ullam aut, sunt repudiandae nisi? Ad expedita explicabo architecto sapiente voluptas atque, rem aperiam dolor fugiat asperiores.
             </p>
-            <p>
+            <p className = 'answer'>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, illo repudiandae! Velit repudiandae, culpa odit quibusdam nam assumenda quis eveniet, voluptas, distinctio provident soluta debitis optio tempora! Minus, harum eaque?
               Sunt, sapiente omnis, suscipit optio quo numquam debitis quisquam voluptatum quibusdam ad harum veniam sequi? Sapiente tempora mollitia omnis quos quis unde expedita, quae pariatur, doloremque maiores suscipit, atque nulla.
               Quae harum ab delectus beatae suscipit impedit, modi non ex optio eaque id ullam aut, sunt repudiandae nisi? Ad expedita explicabo architecto sapiente voluptas atque, rem aperiam dolor fugiat asperiores.
             </p>
-            <p>
+            <p className = 'answer'>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, illo repudiandae! Velit repudiandae, culpa odit quibusdam nam assumenda quis eveniet, voluptas, distinctio provident soluta debitis optio tempora! Minus, harum eaque?
               Sunt, sapiente omnis, suscipit optio quo numquam debitis quisquam voluptatum quibusdam ad harum veniam sequi? Sapiente tempora mollitia omnis quos quis unde expedita, quae pariatur, doloremque maiores suscipit, atque nulla.
               Quae harum ab delectus beatae suscipit impedit, modi non ex optio eaque id ullam aut, sunt repudiandae nisi? Ad expedita explicabo architecto sapiente voluptas atque, rem aperiam dolor fugiat asperiores.
--- src/Components/FAQ/FAQ.js
+++ src/Components/FAQ/FAQ.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 export default function FAQ () {
   return (
-    <main>
+    <main className = 'dashboard_wrapper'>
+    <section className = 'sub_section'>
       <div>
-        <h1>Why does shipping cost so much? </h1>
-        <p>
+        <h1 className = 'question'>Why does shipping cost so much? </h1>
+        <p className = 'answer'>
           Excellent question! As we are shipping live animals, 
           the only method we us is Next-Day Priority shipping. 
           If multiple animals are being sent, we will package
@@ -13,53 +14,43 @@ export default function FAQ () {
         </p>
       </div>
       <div>
-        <h1>How much wood could a woodchuck chuck? </h1>
-        <p>
+        <h1 className = 'question'>How much wood could a woodchuck chuck? </h1>
+        <p className = 'answer'>
           A woodchuck would chuck as much wood as a woodchuck could chuck if a woodchuck could chuck wood.
         </p>
       </div>
       <div>
-        <h1>What does the sound of green taste like?</h1>
-        <p>
+        <h1 className = 'question'> If you raise chickens, does that make you a chicken tender? </h1>
+        <p className = 'answer'>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos alias odio ipsa delectus optio modi minima quas eveniet totam voluptate animi similique vel, exercitationem minus dolor quod in placeat cupiditate.
         </p>
       </div>
       <div>
-        <h1> If you raise chickens, does that make you a chicken tender? </h1>
-        <p>
+        <h1 className = 'question'> What is the air-speed velocity of an unladen swallow? </h1>
+        <p className = 'answer'>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos alias odio ipsa delectus optio modi minima quas eveniet totam voluptate animi similique vel, exercitationem minus dolor quod in placeat cupiditate.
         </p>
       </div>
       <div>
-        <h1> What is the air-speed velocity of an unladen swallow? </h1>
-        <p>
+        <h1 className = 'question'> Are you suggesting that coconuts migrate? </h1>
+        <p className = 'answer'>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos alias odio ipsa delectus optio modi minima quas eveniet totam voluptate animi similique vel, exercitationem minus dolor quod in placeat cupiditate.
         </p>
       </div>
       <div>
-        <h1> Are you suggesting that coconuts migrate? </h1>
-        <p>
+        <h1 className = 'question'> What is the capital of Assyria?  </h1>
+        <p className = 'answer'>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos alias odio ipsa delectus optio modi minima quas eveniet totam voluptate animi similique vel, exercitationem minus dolor quod in placeat cupiditate.
         </p>
       </div>
       <div>
-        <h1> What is the capital of Assyria?  </h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos alias odio ipsa delectus optio modi minima quas eveniet totam voluptate animi similique vel, exercitationem minus dolor quod in placeat cupiditate.
-        </p>
-      </div>
-      <div>
-        <h1> What's in the box? </h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos alias odio ipsa delectus optio modi minima quas eveniet totam voluptate animi similique vel, exercitationem minus dolor quod in placeat cupiditate.
-        </p>
-      </div>
-      <div>
-        <h1> Who ya gonna call? </h1>
-        <p>
+        <h1 className = 'question'> Who ya gonna call? </h1>
+        <p className = 'answer'>
           Ghostbusters!!!
         </p>
       </div>
+    </section>
+      
     </main>
   )
 }
